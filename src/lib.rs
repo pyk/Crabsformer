2a07148a4ec46370add4f4a79d7d86cb95606d5b
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2019-02-23 20:07:08 +0700

    Implement Vec::with_shape(xx).fill(yy)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+extern crate num;
+
+use num::Num;
+
 pub fn zeros(shape: [usize; 2]) -> Vec<Vec<i64>> {
     let mut rows = Vec::with_capacity(shape[0]);
     for _ in 0..shape[0] {
@@ -10,50 +14,74 @@ pub fn zeros(shape: [usize; 2]) -> Vec<Vec<i64>> {
     rows
 }
 
-pub trait Array1D {
-    // Array creation routines
-    fn zeros(shape: usize) -> Self;
+trait Size<T> where T: Num + Copy {
+    fn with_size(size: usize) -> Vec<T>;
 }
 
-impl Array1D for Vec<i64> {
-    fn zeros(shape: usize) -> Vec<i64> {
-        let mut array = Vec::with_capacity(shape);
-        for _ in 0..shape {
-            array.push(0);
-        }
-        array
+impl<T> Size<T> for Vec<T> where T: Num + Copy {
+    fn with_size(size: usize) -> Vec<T> {
+        Vec::with_capacity(size)
     }
 }
 
-impl Array1D for Vec<i32> {
-    fn zeros(shape: usize) -> Vec<i32> {
-        let mut array = Vec::with_capacity(shape);
-        for _ in 0..shape {
-            array.push(0);
+trait Shape<T> where T: Num + Copy {
+    // TODO: don't use slice.s
+    fn with_shape(shape: &[usize]) -> Self;
+}
+
+impl<T> Shape<T> for Vec<Vec<T>> where T: Num + Copy {
+    fn with_shape(shape: &[usize]) -> Vec<Vec<T>> {
+        let mut rows: Vec<Vec<T>> = Vec::with_capacity(shape[0]);
+        for _ in 0..shape[0] {
+            rows.push(Vec::with_capacity(shape[1]));
         }
-        array
+        rows
     }
 }
 
-impl Array1D for Vec<f64> {
-    fn zeros(shape: usize) -> Vec<f64> {
-        let mut array = Vec::with_capacity(shape);
-        for _ in 0..shape {
-            array.push(0.0);
+impl<T> Shape<T> for Vec<Vec<Vec<T>>> where T: Num + Copy {
+    fn with_shape(shape: &[usize]) -> Vec<Vec<Vec<T>>> {
+        let mut rows: Vec<Vec<Vec<T>>> = Vec::with_capacity(shape[0]);
+        for _ in 0..shape[0] {
+            let mut cols: Vec<Vec<T>> = Vec::with_capacity(shape[1]);
+            for _ in 0..shape[1] {
+                cols.push(Vec::with_capacity(shape[2]));
+            }
+            rows.push(cols);
         }
-        array
+        rows
     }
 }
 
-impl Array1D for Vec<f32> {
-    fn zeros(shape: usize) -> Vec<f32> {
-        let mut array = Vec::with_capacity(shape);
-        for _ in 0..shape {
-            array.push(0.0);
+// trait Shape4D<T> where T: Num + Copy {
+//     fn with_shape(shape: [usize; 4]) -> Vec<Vec<Vec<Vec<T>>>>;
+// }
+
+
+trait Fill<T> where T: Num + Copy {
+    fn fill(&mut self, value: T) -> Self;
+}
+
+impl<T> Fill<T> for Vec<T> where T: Num + Copy {
+    fn fill(&mut self, value: T) -> Vec<T> {
+        for _ in 0..self.capacity() {
+            self.push(value);
         }
-        array
+        self.to_vec()
     }
 }
+
+impl<T> Fill<T> for Vec<Vec<T>> where T: Num + Copy {
+    fn fill(&mut self, value: T) -> Vec<Vec<T>> {
+        for i in 0..self.capacity() {
+            for _ in 0..self[i].capacity() {
+                self[i].push(value);
+            }
+        }
+        self.to_vec()
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -69,19 +97,40 @@ mod tests {
     }
 
     #[test]
-    fn test_zeros() {
+    fn test_with_size() {
+        let arr: Vec<i32> = Vec::with_size(5);
+        assert_eq!(arr.capacity(), 5);
+
+        let arr: Vec<i64> = Vec::with_size(5);
+        assert_eq!(arr.capacity(), 5);
+    }
+
+
+    #[test]
+    fn test_with_shape() {
+        let arr1: Vec<Vec<i32>> = Vec::with_shape(&[5, 5]);
+        assert_eq!(arr1.capacity(), 5);
+        for i in 0..5 {
+            assert_eq!(arr1[i].capacity(), 5);
+        }
+
+        let arr2: Vec<Vec<u64>> = Vec::with_shape(&[5, 10]);
+        assert_eq!(arr2.capacity(), 5);
+        for i in 0..5 {
+            assert_eq!(arr2[i].capacity(), 10);
+        }
+    }
+
+    #[test]
+    fn test_fill() {
         // 1D array
-        let array_1d_i64: Vec<i64> = Vec::zeros(5);
-        assert_eq!(array_1d_i64, vec![0, 0, 0, 0, 0]);
+        let arr1: Vec<i32> = Vec::with_size(5).fill(0);
+        assert_eq!(arr1, vec![0, 0, 0, 0, 0]);
 
-        let array_1d_i32: Vec<i32> = Vec::zeros(5);
-        assert_eq!(array_1d_i32, vec![0, 0, 0, 0, 0]);
-
-        let array_1d_f64: Vec<f64> = Vec::zeros(5);
-        assert_eq!(array_1d_f64, vec![0.0, 0.0, 0.0, 0.0, 0.0]);
-
-        let array_1d_f32: Vec<f32> = Vec::zeros(5);
-        assert_eq!(array_1d_f32, vec![0.0, 0.0, 0.0, 0.0, 0.0]);
-
+        let arr2: Vec<Vec<f64>> = Vec::with_shape(&[2, 2]).fill(5.0);
+        assert_eq!(arr2, vec![
+            vec![5.0, 5.0],
+            vec![5.0, 5.0],
+        ]);
     }
 }
