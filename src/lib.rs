c1e3d6b948428be4f3db7fab1c2dcb9f1de32941
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2019-03-01 18:38:40 +0700

    Remove Array<T> and update tests

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,6 @@
 extern crate num;
 
 use num::Num;
-use std::ops::Index;
-use std::slice::SliceIndex;
 
 
 pub fn zeros(shape: [usize; 2]) -> Vec<Vec<i64>> {
@@ -35,23 +33,6 @@ pub fn zeros(shape: [usize; 2]) -> Vec<Vec<i64>> {
     rows
 }
 
-struct Array<T> {
-    ndim: usize,
-    shape: Vec<usize>,
-    data: Vec<T>
-}
-
-impl<T> Index<usize> for Array<T> {
-    type Output = T;
-
-    fn index(&self, index: usize) -> &T {
-        &self.data[index]
-        // Index::index(&*self, index)
-    }
-}
-
-
-
 /// One-dimensional vectors
 pub trait OneDimensional<T> where T: Num + Copy {
     /// Returns new one-dimensional vector with specified `size`.
@@ -64,44 +45,6 @@ impl<T> OneDimensional<T> for Vec<T> where T: Num + Copy {
         Vec::with_capacity(size)
     }
 }
-
-/// One-dimensional vectors
-pub trait OneDimensionalArray<T> where T: Num + Copy {
-    /// Returns new one-dimensional vector with specified `size`.
-    // TODO(pyk): Add example
-    fn one_dim_array(size: usize) -> Self;
-}
-
-impl<T> OneDimensionalArray<T> for Array<T> where T: Num + Copy {
-    fn one_dim_array(size: usize) -> Array<T> {
-        Array {
-            ndim: 1,
-            shape: vec![size],
-            data: Vec::with_capacity(size)
-        }
-    }
-}
-
-
-/// Two-dimensional vectors
-pub trait TwoDimensionalArray<T> where T: Num + Copy {
-    /// Returns new two-dimensional vector with specified
-    /// number of rows and number of columns.
-    ///
-    // TODO(pyk): Add example
-    fn two_dim_array(nrows: usize, ncols: usize) -> Self;
-}
-
-impl<T> TwoDimensionalArray<T> for Array<T> where T: Num + Copy {
-    fn two_dim_array(nrows: usize, ncols: usize) -> Array<T> {
-        Array {
-            ndim: 2,
-            shape: vec![nrows, ncols],
-            data: Vec::new()
-        }
-    }
-}
-
 
 /// Two-dimensional vectors
 pub trait TwoDimensional<T> where T: Num + Copy {
@@ -241,28 +184,6 @@ impl<T> Full<T> for Vec<Vec<Vec<Vec<T>>>> where T: Num + Copy {
             }
         }
         self.to_vec()
-    }
-}
-
-/// Fillable vectors
-pub trait FullArray<T> where T: Num + Copy {
-    /// Returns a new array of given shape and type, filled with `value`.
-    fn full_array(self, value: T) -> Self;
-}
-
-impl<T> FullArray<T> for Array<T> where T: Num + Copy {
-    fn full_array(self, value: T) -> Array<T> {
-        // Create new Array with new value
-        let capacity = self.shape.iter().product();
-        let mut new_data = Vec::with_capacity(capacity);
-        for _ in 0..new_data.capacity() {
-            new_data.push(value);
-        }
-        Array {
-            shape: self.shape,
-            ndim: self.ndim,
-            data: new_data
-        }
     }
 }
 
@@ -564,21 +485,6 @@ impl Zero for Vec<Vec<Vec<Vec<f64>>>> {
 }
 
 
-/// A zero-able vectors
-pub trait ZeroArray {
-    /// Return a new vector of given data type and shape,
-    /// filled with zeros.
-    fn zeros_array(self) -> Self;
-}
-
-impl ZeroArray for Array<i32> {
-    fn zeros_array(self) -> Array<i32> {
-        self.full_array(0)
-    }
-}
-
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -586,9 +492,9 @@ mod tests {
     #[test]
     fn test_fn_zeros() {
         let arr2d: Vec<Vec<i64>> = zeros([2, 2]);
-        assert_eq!(arr2d, vec![
-            vec![0, 0],
-            vec![0, 0],
+        assert_eq!(arr2d, [
+            [0, 0],
+            [0, 0],
         ])
     }
 
@@ -639,360 +545,312 @@ mod tests {
     fn test_full_one_dim() {
         // 1D array
         let arr1: Vec<i32> = Vec::one_dim(5).full(0);
-        assert_eq!(arr1, vec![0, 0, 0, 0, 0]);
+        assert_eq!(arr1, [0, 0, 0, 0, 0]);
     }
 
     #[test]
     fn test_full_two_dim() {
         let arr2: Vec<Vec<f64>> = Vec::two_dim(1, 2).full(5.0);
-        assert_eq!(arr2, vec![vec![5.0, 5.0]]);
+        assert_eq!(arr2, [[5.0, 5.0]]);
     }
 
     #[test]
     fn test_full_three_dim() {
         let arr3: Vec<Vec<Vec<f64>>> = Vec::three_dim(1, 1, 2).full(5.0);
-        assert_eq!(arr3, vec![vec![vec![5.0, 5.0]]]);
+        assert_eq!(arr3, [[[5.0, 5.0]]]);
     }
 
     #[test]
     fn test_full_four_dim() {
         let arr4: Vec<Vec<Vec<Vec<f64>>>> = Vec::four_dim(1, 1, 1, 2).full(5.0);
-        assert_eq!(arr4, vec![vec![vec![vec![5.0, 5.0]]]]);
+        assert_eq!(arr4, [[[[5.0, 5.0]]]]);
     }
 
     #[test]
     fn test_zeros_u8_one_dim() {
         let arr: Vec<u8> = Vec::one_dim(2).zeros();
-        assert_eq!(arr, vec![0, 0]);
+        assert_eq!(arr, [0, 0]);
     }
 
     #[test]
     fn test_zeros_u8_two_dim() {
         let arr: Vec<Vec<u8>> = Vec::two_dim(1, 2).zeros();
-        assert_eq!(arr, vec![vec![0, 0]]);
+        assert_eq!(arr, [[0, 0]]);
     }
 
     #[test]
     fn test_zeros_u8_three_dim() {
         let arr: Vec<Vec<Vec<u8>>> = Vec::three_dim(1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![0, 0]]]);
+        assert_eq!(arr, [[[0, 0]]]);
     }
 
     #[test]
     fn test_zeros_u8_four_dim() {
         let arr: Vec<Vec<Vec<Vec<u8>>>> = Vec::four_dim(1, 1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![vec![0, 0]]]]);
+        assert_eq!(arr, [[[[0, 0]]]]);
     }
 
     #[test]
     fn test_zeros_u16_one_dim() {
         let arr: Vec<u16> = Vec::one_dim(2).zeros();
-        assert_eq!(arr, vec![0, 0]);
+        assert_eq!(arr, [0, 0]);
     }
 
     #[test]
     fn test_zeros_u16_two_dim() {
         let arr: Vec<Vec<u16>> = Vec::two_dim(1, 2).zeros();
-        assert_eq!(arr, vec![vec![0, 0]]);
+        assert_eq!(arr, [[0, 0]]);
     }
 
     #[test]
     fn test_zeros_u16_three_dim() {
         let arr: Vec<Vec<Vec<u16>>> = Vec::three_dim(1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![0, 0]]]);
+        assert_eq!(arr, [[[0, 0]]]);
     }
 
     #[test]
     fn test_zeros_u16_four_dim() {
         let arr: Vec<Vec<Vec<Vec<u16>>>> = Vec::four_dim(1, 1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![vec![0, 0]]]]);
+        assert_eq!(arr, [[[[0, 0]]]]);
     }
 
     #[test]
     fn test_zeros_u32_one_dim() {
         let arr: Vec<u32> = Vec::one_dim(2).zeros();
-        assert_eq!(arr, vec![0, 0]);
+        assert_eq!(arr, [0, 0]);
     }
 
     #[test]
     fn test_zeros_u32_two_dim() {
         let arr: Vec<Vec<u32>> = Vec::two_dim(1, 2).zeros();
-        assert_eq!(arr, vec![vec![0, 0]]);
+        assert_eq!(arr, [[0, 0]]);
     }
 
     #[test]
     fn test_zeros_u32_three_dim() {
         let arr: Vec<Vec<Vec<u32>>> = Vec::three_dim(1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![0, 0]]]);
+        assert_eq!(arr, [[[0, 0]]]);
     }
 
     #[test]
     fn test_zeros_u32_four_dim() {
         let arr: Vec<Vec<Vec<Vec<u32>>>> = Vec::four_dim(1, 1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![vec![0, 0]]]]);
+        assert_eq!(arr, [[[[0, 0]]]]);
     }
 
     #[test]
     fn test_zeros_u64_one_dim() {
         let arr: Vec<u64> = Vec::one_dim(2).zeros();
-        assert_eq!(arr, vec![0, 0]);
+        assert_eq!(arr, [0, 0]);
     }
 
     #[test]
     fn test_zeros_u64_two_dim() {
         let arr: Vec<Vec<u64>> = Vec::two_dim(1, 2).zeros();
-        assert_eq!(arr, vec![vec![0, 0]]);
+        assert_eq!(arr, [[0, 0]]);
     }
 
     #[test]
     fn test_zeros_u64_three_dim() {
         let arr: Vec<Vec<Vec<u64>>> = Vec::three_dim(1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![0, 0]]]);
+        assert_eq!(arr, [[[0, 0]]]);
     }
 
     #[test]
     fn test_zeros_u64_four_dim() {
         let arr: Vec<Vec<Vec<Vec<u64>>>> = Vec::four_dim(1, 1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![vec![0, 0]]]]);
+        assert_eq!(arr, [[[[0, 0]]]]);
     }
 
     #[test]
     fn test_zeros_u128_one_dim() {
         let arr: Vec<u128> = Vec::one_dim(2).zeros();
-        assert_eq!(arr, vec![0, 0]);
+        assert_eq!(arr, [0, 0]);
     }
 
     #[test]
     fn test_zeros_u128_two_dim() {
         let arr: Vec<Vec<u128>> = Vec::two_dim(1, 2).zeros();
-        assert_eq!(arr, vec![vec![0, 0]]);
+        assert_eq!(arr, [[0, 0]]);
     }
 
     #[test]
     fn test_zeros_u128_three_dim() {
         let arr: Vec<Vec<Vec<u128>>> = Vec::three_dim(1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![0, 0]]]);
+        assert_eq!(arr, [[[0, 0]]]);
     }
 
     #[test]
     fn test_zeros_u128_four_dim() {
         let arr: Vec<Vec<Vec<Vec<u128>>>> = Vec::four_dim(1, 1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![vec![0, 0]]]]);
+        assert_eq!(arr, [[[[0, 0]]]]);
     }
 
     #[test]
     fn test_zeros_i8_one_dim() {
         let arr: Vec<i8> = Vec::one_dim(2).zeros();
-        assert_eq!(arr, vec![0, 0]);
+        assert_eq!(arr, [0, 0]);
     }
 
     #[test]
     fn test_zeros_i8_two_dim() {
         let arr: Vec<Vec<i8>> = Vec::two_dim(1, 2).zeros();
-        assert_eq!(arr, vec![vec![0, 0]]);
+        assert_eq!(arr, [[0, 0]]);
     }
 
     #[test]
     fn test_zeros_i8_three_dim() {
         let arr: Vec<Vec<Vec<i8>>> = Vec::three_dim(1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![0, 0]]]);
+        assert_eq!(arr, [[[0, 0]]]);
     }
 
     #[test]
     fn test_zeros_i8_four_dim() {
         let arr: Vec<Vec<Vec<Vec<i8>>>> = Vec::four_dim(1, 1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![vec![0, 0]]]]);
+        assert_eq!(arr, [[[[0, 0]]]]);
     }
 
     #[test]
     fn test_zeros_i16_one_dim() {
         let arr: Vec<i16> = Vec::one_dim(2).zeros();
-        assert_eq!(arr, vec![0, 0]);
+        assert_eq!(arr, [0, 0]);
     }
 
     #[test]
     fn test_zeros_i16_two_dim() {
         let arr: Vec<Vec<i16>> = Vec::two_dim(1, 2).zeros();
-        assert_eq!(arr, vec![vec![0, 0]]);
+        assert_eq!(arr, [[0, 0]]);
     }
 
     #[test]
     fn test_zeros_i16_three_dim() {
         let arr: Vec<Vec<Vec<i16>>> = Vec::three_dim(1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![0, 0]]]);
+        assert_eq!(arr, [[[0, 0]]]);
     }
 
     #[test]
     fn test_zeros_i16_four_dim() {
         let arr: Vec<Vec<Vec<Vec<i16>>>> = Vec::four_dim(1, 1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![vec![0, 0]]]]);
+        assert_eq!(arr, [[[[0, 0]]]]);
     }
 
     #[test]
     fn test_zeros_i32_one_dim() {
         let arr: Vec<i32> = Vec::one_dim(2).zeros();
-        assert_eq!(arr, vec![0, 0]);
+        assert_eq!(arr, [0, 0]);
     }
 
     #[test]
     fn test_zeros_i32_two_dim() {
         let arr: Vec<Vec<i32>> = Vec::two_dim(1, 2).zeros();
-        assert_eq!(arr, vec![vec![0, 0]]);
+        assert_eq!(arr, [[0, 0]]);
     }
 
     #[test]
     fn test_zeros_i32_three_dim() {
         let arr: Vec<Vec<Vec<i32>>> = Vec::three_dim(1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![0, 0]]]);
+        assert_eq!(arr, [[[0, 0]]]);
     }
 
     #[test]
     fn test_zeros_i32_four_dim() {
         let arr: Vec<Vec<Vec<Vec<i32>>>> = Vec::four_dim(1, 1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![vec![0, 0]]]]);
+        assert_eq!(arr, [[[[0, 0]]]]);
     }
 
     #[test]
     fn test_zeros_i64_one_dim() {
         let arr: Vec<i64> = Vec::one_dim(2).zeros();
-        assert_eq!(arr, vec![0, 0]);
+        assert_eq!(arr, [0, 0]);
     }
 
     #[test]
     fn test_zeros_i64_two_dim() {
         let arr: Vec<Vec<i64>> = Vec::two_dim(1, 2).zeros();
-        assert_eq!(arr, vec![vec![0, 0]]);
+        assert_eq!(arr, [[0, 0]]);
     }
 
     #[test]
     fn test_zeros_i64_three_dim() {
         let arr: Vec<Vec<Vec<i64>>> = Vec::three_dim(1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![0, 0]]]);
+        assert_eq!(arr, [[[0, 0]]]);
     }
 
     #[test]
     fn test_zeros_i64_four_dim() {
         let arr: Vec<Vec<Vec<Vec<i64>>>> = Vec::four_dim(1, 1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![vec![0, 0]]]]);
+        assert_eq!(arr, [[[[0, 0]]]]);
     }
 
     #[test]
     fn test_zeros_i128_one_dim() {
         let arr: Vec<i128> = Vec::one_dim(2).zeros();
-        assert_eq!(arr, vec![0, 0]);
+        assert_eq!(arr, [0, 0]);
     }
 
     #[test]
     fn test_zeros_i128_two_dim() {
         let arr: Vec<Vec<i128>> = Vec::two_dim(1, 2).zeros();
-        assert_eq!(arr, vec![vec![0, 0]]);
+        assert_eq!(arr, [[0, 0]]);
     }
 
     #[test]
     fn test_zeros_i128_three_dim() {
         let arr: Vec<Vec<Vec<i128>>> = Vec::three_dim(1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![0, 0]]]);
+        assert_eq!(arr, [[[0, 0]]]);
     }
 
     #[test]
     fn test_zeros_i128_four_dim() {
         let arr: Vec<Vec<Vec<Vec<i128>>>> = Vec::four_dim(1, 1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![vec![0, 0]]]]);
+        assert_eq!(arr, [[[[0, 0]]]]);
     }
 
     #[test]
     fn test_zeros_f32_one_dim() {
         let arr: Vec<f32> = Vec::one_dim(2).zeros();
-        assert_eq!(arr, vec![0.0, 0.0]);
+        assert_eq!(arr, [0.0, 0.0]);
     }
 
     #[test]
     fn test_zeros_f32_two_dim() {
         let arr: Vec<Vec<f32>> = Vec::two_dim(1, 2).zeros();
-        assert_eq!(arr, vec![vec![0.0, 0.0]]);
+        assert_eq!(arr, [[0.0, 0.0]]);
     }
 
     #[test]
     fn test_zeros_f32_three_dim() {
         let arr: Vec<Vec<Vec<f32>>> = Vec::three_dim(1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![0.0, 0.0]]]);
+        assert_eq!(arr, [[[0.0, 0.0]]]);
     }
 
     #[test]
     fn test_zeros_f32_four_dim() {
         let arr: Vec<Vec<Vec<Vec<f32>>>> = Vec::four_dim(1, 1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![vec![0.0, 0.0]]]]);
+        assert_eq!(arr, [[[[0.0, 0.0]]]]);
     }
 
     #[test]
     fn test_zeros_f64_one_dim() {
         let arr: Vec<f64> = Vec::one_dim(2).zeros();
-        assert_eq!(arr, vec![0.0, 0.0]);
+        assert_eq!(arr, [0.0, 0.0]);
     }
 
     #[test]
     fn test_zeros_f64_two_dim() {
         let arr: Vec<Vec<f64>> = Vec::two_dim(1, 2).zeros();
-        assert_eq!(arr, vec![vec![0.0, 0.0]]);
+        assert_eq!(arr, [[0.0, 0.0]]);
     }
 
     #[test]
     fn test_zeros_f64_three_dim() {
         let arr: Vec<Vec<Vec<f64>>> = Vec::three_dim(1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![0.0, 0.0]]]);
+        assert_eq!(arr, [[[0.0, 0.0]]]);
     }
 
     #[test]
     fn test_zeros_f64_four_dim() {
         let arr: Vec<Vec<Vec<Vec<f64>>>> = Vec::four_dim(1, 1, 1, 2).zeros();
-        assert_eq!(arr, vec![vec![vec![vec![0.0, 0.0]]]]);
-    }
-
-    #[test]
-    fn test_array_one_dim() {
-        let arr: Array<i32> = Array::one_dim_array(5);
-        assert_eq!(arr.ndim, 1);
-        assert_eq!(arr.shape, [5]);
-    }
-
-    #[test]
-    fn test_array_one_dim_zeros() {
-        let arr: Array<i32> = Array::one_dim_array(5).zeros_array();
-        assert_eq!(arr.ndim, 1);
-        assert_eq!(arr.shape, [5]);
-        assert_eq!(arr.data, [0, 0, 0, 0, 0]);
-    }
-
-    #[test]
-    fn test_array_one_dim_zeros_index() {
-        let arr: Array<i32> = Array::one_dim_array(5).zeros_array();
-        for i in 0..5 {
-            assert_eq!(arr[i], 0);
-        }
-    }
-
-    #[test]
-    fn test_array_two_dim() {
-        let arr: Array<i32> = Array::two_dim_array(5, 5);
-        assert_eq!(arr.ndim, 2);
-        assert_eq!(arr.shape, [5, 5]);
-    }
-
-    #[test]
-    fn test_array_two_dim_zeros() {
-        let arr: Array<i32> = Array::two_dim_array(2, 2).zeros_array();
-        assert_eq!(arr.ndim, 2);
-        assert_eq!(arr.shape, [2, 2]);
-        assert_eq!(arr.data, [0, 0, 0, 0]);
-    }
-
-    #[test]
-    fn test_array_two_dim_zeros_index() {
-        let arr: Array<i32> = Array::two_dim_array(2, 2).zeros_array();
-        for i in 0..2 {
-            for j in 0..2 {
-                assert_eq!(arr[i][j], 0);
-            }
-        }
+        assert_eq!(arr, [[[[0.0, 0.0]]]]);
     }
 }
